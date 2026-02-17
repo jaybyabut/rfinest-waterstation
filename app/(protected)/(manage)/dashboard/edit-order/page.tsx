b5462a68cf0d10c7b2d3775b1bd68215dae19bf6
Januard Esguerra <esguerrajanuarddd@gmail.com>
2026-02-17 11:37:07 +0800

    feat: edit-order UI complete, ready for backend integration

---
+++ app/(protected)/(manage)/dashboard/edit-order/page.tsx
@@ -0,0 +1,9 @@
+import EditOrderForm from "@/components/admin/edit-order";
+
+export default function EditOrderPage(){
+    return(
+        <div className="flex-1 w-full flex flex-col items-center justify-center">
+            <EditOrderForm />
+        </div>
+    );
+}
