e97343cdc64d4f28d809e6d8221c5b5181ff631e
Januard Esguerra <esguerrajanuarddd@gmail.com>
2026-02-17 14:57:39 +0800

    feat: manage prices UI complete with search and bulk update logic - ready for backend

---
+++ app/(protected)/(manage)/dashboard/manage-prices/page.tsx
@@ -0,0 +1,9 @@
+import ManagePricesForm from "@/components/admin/manage-orders"
+
+export default function ManagePrices(){
+    return(
+        <div className="flex-1 w-full flex flex-col items-center justify-center">
+            <ManagePricesForm />
+        </div>
+    );
+}
