428649bddea1495466a4e4d5dca83121b7706d65
xiovy <xionexgm23@gmail.com>
2026-03-11 16:42:13 +0800

    fixed tablet routing and updated UI design

---
+++ app/(protected)/(station)/live-queue/employeeTablet/page.tsx
@@ -0,0 +1,9 @@
+import EmployeeTablet from "@/components/station/employeeTablet/employeeTablet";
+
+export default function EmployeeTabletPage() {
+  return (
+    <main className="h-screen w-full">
+      <EmployeeTablet />
+    </main>
+  );
+}
