0d676367cd6b7d50da1acda24a2c1c6adde68fad
jaybyabut <jaybyabut@gmail.com>
2026-03-24 23:58:42 +0800

    test fix

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,15 +2,9 @@
 import { createOnlineOrder } from "@/app/actions/createOnlineOrder";
 
 export default function TestPage() {
-    const handleTest = async () => {
-        const result = await createOnlineOrder({
-            slimCount: 1,
-            roundCount: 1,
-            payment_mode: "Cash",
-            transaction_type: "Online"
-        });
-        console.log(result);
-    };
+   const handleTest = () => {
+    console.log('hello');
+   }
 
     return <button onClick={handleTest}>Run Server Action</button>;
 }
